e594e437e2f6491311aeb5a2d4da1f78ac607a80
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-11-27 19:08:55 -0500

    Diseño de interfaces

---
+++ Navegacion/Pantalla2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Pantalla2ViewController.swift
+//  Navegacion
+//
+//  Created by Aplimovil on 11/27/15.
+//  Copyright © 2015 Aplimovil. All rights reserved.
+//
+
+import UIKit
+
+class Pantalla2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
